c580342bf63f47f93f91b99d2775fa6c7aac06b1
MAMMARU-class <144889491+MAMMARU-class@users.noreply.github.com>
2026-01-20 15:41:53 +0900

    update few

--- tests/WiFi_Server/src/main.cpp
+++ tests/WiFi_Server/src/main.cpp
@@ -3,7 +3,7 @@
 
 const char ssid[] = "E-PIX_WiFi";
 const char pass[] = "E-PIX_WiFi";
-const IPAddress ip(192, 168, 11, 3);
+const IPAddress ip(192, 168, 3, 11);
 const IPAddress subnet(255, 255, 255, 0);
 WiFiServer server(80);
 
@@ -21,14 +21,14 @@ void setup() {
     Serial.print("IP address = ");
     Serial.println(myIP);
     Serial.println("server started");
-    neopixelWrite(RGB_BUILTIN, 155, 0, 0);
+    neopixelWrite(RGB_BUILTIN,255, 0, 0);
 }
 
 void loop() {
     WiFiClient client = server.available();
     if (client) {
         Serial.println("new client");
-        neopixelWrite(RGB_BUILTIN, 0, 155, 0);
+        neopixelWrite(RGB_BUILTIN, 0, 255, 0);
         String inMsg = "";
         while (client.connected()) {
             if (client.available()) {
@@ -54,6 +54,6 @@ void loop() {
         delay(5000);
         client.stop();
         Serial.println("client disconnected");
-        neopixelWrite(RGB_BUILTIN, 155, 0, 0);
+        neopixelWrite(RGB_BUILTIN, 255, 0, 0);
     }
 }
