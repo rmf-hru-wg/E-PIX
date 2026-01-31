27d8977ee8ac29d6efa39debf777b232a6d000bc
MAMMARU-class <144889491+MAMMARU-class@users.noreply.github.com>
2026-01-31 18:33:05 +0900

    check all functions. Confirm both serial1 and serial2

--- tests/Motor/src/main.cpp
+++ tests/Motor/src/main.cpp
@@ -5,23 +5,29 @@
 #define ICS_BAUDRATE 115200
 #define ICS_TIMEOUT 10
 
-#define myEN1 16
+#define myEN1 8
 #define myTX1 17
 #define myRX1 18
 
+#define myEN2 16
+#define myTX2 15
+#define myRX2 7
+
 IcsHardSerialClass krs1(&Serial1, myEN1, ICS_BAUDRATE, ICS_TIMEOUT, myRX1, myTX1);
+IcsHardSerialClass krs2(&Serial2, myEN2, ICS_BAUDRATE, ICS_TIMEOUT, myRX2, myTX2);
 
 void setup() {
     krs1.begin();
+    krs2.begin();
 }
 
 void loop() {
-    krs1.setPos(1,7500);
+    krs2.setPos(1,7500);
     delay(1000);
-    krs1.setPos(1,5500);
+    krs2.setPos(1,5500);
     delay(1000);
-    krs1.setPos(1,7500);
+    krs2.setPos(1,7500);
     delay(1000);
-    krs1.setPos(1,9500);
+    krs2.setPos(1,9500);
     delay(1000);
 }
